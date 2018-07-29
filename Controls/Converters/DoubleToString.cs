3f11b261e7e1ffcc4e4155d0fe8693ecbb1c67f1
Дмитрий Кривченя <krdemetria@ya.ru>
2018-07-29 16:00:04 +0300

    Библиотека вспомогательных контролов.

---
+++ Controls/Converters/DoubleToString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Controls.Converters
+{
+    class DoubleToString : ConvertorBase<DoubleToString>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (double)value;
+            string res = string.Empty;
+            if (parameter is string)
+                res = val.ToString((string)parameter);
+            else
+                res = val.ToString();
+            return res;
+        }
+    }
+}
