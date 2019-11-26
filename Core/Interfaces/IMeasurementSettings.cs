7cf02498191d6e37bde4344de3cd2c21b81ded08
Дмитрий Кривченя <krdemetria@ya.ru>
2019-11-26 09:56:28 +0300

    Добавил отсутствующие файлы к предыдущему изменению.

---
+++ Core/Interfaces/IMeasurementSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Interfaces
+{
+	public interface IMeasurementSettings
+	{
+		/// <summary>
+		/// Значение напряжения в мВ для начала измерения
+		/// </summary>
+		double From { get; set; }
+
+		/// <summary>
+		/// Значение напряжения в мВ для окончания измерения
+		/// </summary>
+		double To { get; set; }
+
+		/// <summary>
+		/// Значение напряжения шага измерения в мВ.
+		/// По умолчанию значение равно 0,1 мВ.
+		/// НЕ может быть меньше двойного значения <see cref="MeasurementCore.PointRange"/>.
+		/// </summary>
+		double Step { get; set; }
+
+		/// <summary>
+		/// Диапазон в мВ для точки измерения <see cref="MeasurementCore.Next"/>, при попадании в который производится измерения.
+		/// Минимальное значение - 0,010 мВ, максимальное значение - 0,1 мВ. По умолчанию - 0,015 мВ.
+		/// </summary>
+		double PointRange { get; set; }
+	}
+}
---
+++ Core/MeasurementSettings.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.Interfaces;
+
+namespace Core
+{
+	public class MeasurementSettings : IMeasurementSettings
+	{
+		double _from = 0;
+		double _pointRange = 0.05;
+		double _step = 0.1;
+
+		/// <summary>
+		/// Значение напряжения в мВ для начала измерения
+		/// </summary>
+		public double From
+		{
+			get { return _from; }
+			set
+			{
+				_from = value;
+			}
+		}
+
+		/// <summary>
+		/// Значение напряжения в мВ для окончания измерения
+		/// </summary>
+		public double To { get; set; }
+
+		/// <summary>
+		/// Значение напряжения шага измерения в мВ.
+		/// По умолчанию значение равно 0,1 мВ.
+		/// НЕ может быть меньше двойного значения <see cref="MeasurementCore.PointRange"/>.
+		/// </summary>
+		public double Step
+		{
+			get { return _step; }
+			set
+			{
+				if (value <= PointRange)
+					value = 2 * PointRange;
+				_step = value;
+			}
+		}
+
+		/// <summary>
+		/// Диапазон в мВ для точки измерения <see cref="MeasurementCore.Next"/>, при попадании в который производится измерения.
+		/// Минимальное значение - 0,010 мВ, максимальное значение - 0,1 мВ. По умолчанию - 0,015 мВ.
+		/// </summary>
+		public double PointRange
+		{
+			get { return _pointRange; }
+			set
+			{
+				if (value < 0.010)
+					value = 0.010;
+				if (value > 0.1)
+					value = 0.1;
+				_pointRange = value;
+			}
+		}
+	}
+}
