fb4bd3e7a7102e7cd31c7864538e2c8b07dd2bf8
Дмитрий Кривченя <krdemetria@ya.ru>
2020-03-11 09:52:04 +0300

    Переопределены стандартные методы работы с коллекцией в классе WSICollection. Сделан небольшой рефакторинг.

--- Core/CollectionWithSelectedItem.cs
+++ Core/CollectionWithSelectedItem.cs
@@ -25,17 +25,31 @@ namespace Core
 	/// Коллекция с указанием на текущий элемент
 	/// (WSI - with selected item)
 	/// </summary>
-	public class WSICollection<T> : Collection<T> 
+	public class WSICollection<T> : Collection<T>
 	{
 		public delegate void SelectionChangedDelegate(WSICollection<T> collection, ChangedEventArgs<T> args);
 		public event SelectionChangedDelegate SelectedItemChanged;
 		private int _selectedIndex = -1;
 
 		/// <summary>
+		/// Индекс текущего выделенного элемента коллекции.
+		/// Если не были ни разу вызван метод <see cref="ChangeSelection()"/> или его перегрузки, то возвращает значение -1.
+		/// </summary>
+		public int SelectedIndex
+		{
+			get
+			{
+				return _selectedIndex;
+			}
+		}
+
+		/// <summary>
 		/// Текущий выделенный элемент коллекции
 		/// </summary>
-		public T SelectedItem { 
-			get {
+		public T SelectedItem
+		{
+			get
+			{
 				return _selectedIndex < 0 ? (T)GetDefault() : this[_selectedIndex];
 			}
 		}
@@ -44,46 +58,73 @@ namespace Core
 		/// Изменяет текущий элемент на следующий в коллекции.
 		/// </summary>
 		/// <returns></returns>
-		internal bool ChangeSelection()
+		public bool ChangeSelection()
 		{
 			if (_selectedIndex >= this.Count)
 				return false;
 			return ChangeSelection(_selectedIndex + 1);
 		}
-		
+
 		/// <summary>
 		/// Изменяет текущий элемент на указанный.
 		/// </summary>
 		/// <param name="item"></param>
 		/// <returns></returns>
-		internal bool ChangeSelection(T item)
+		public bool ChangeSelection(T item)
 		{
-			if(this.Count == 0)
+			if (this.Count == 0)
 				return false;
 
-			if(item != null && (!this.Contains(item) || this.SelectedItem.Equals(item)))
+			if (item != null && (!this.Contains(item) || this.SelectedItem.Equals(item)))
 				return false;
 
 			return ChangeSelection(this.IndexOf(item));
 		}
 
 		/// <summary>
-		/// Изменяет текущий этап настройки на этап по его индексу
+		/// Изменяет индекс текущего элемента на указанный индекс.
 		/// </summary>
 		/// <param name="index"></param>
 		/// <returns></returns>
-		internal bool ChangeSelection(int index)
+		public bool ChangeSelection(int index)
 		{
 			if (index < 0 || index >= this.Count)
 				throw new ArgumentOutOfRangeException("index", "Разрешенный диапазон индекса - [0; " + (this.Count - 1) + "].");
 			if (index == _selectedIndex)
 				return false;
 
+			ChangeSelectedIndex(index);
+			return true;
+		}
+
+		/// <summary>
+		/// Обнуляет текущий выделенный индекс.
+		/// </summary>
+		public void ResetSelection()
+		{
+			ChangeSelectedIndex(-1);
+		}
+
+		/// <summary>
+		/// Изменяет текущий выделенный индекс на указанный.
+		/// Для внутренних применений.
+		/// </summary>
+		/// <param name="index"></param>
+		private void ChangeSelectedIndex(int index)
+		{
 			var oldItem = SelectedItem;
 			_selectedIndex = index;
+			RaiseselectedItemChanged(oldItem);
+		}
+
+		/// <summary>
+		/// Генерирует событие <see cref="SelectedItemChanged"/>.
+		/// </summary>
+		/// <param name="oldItem"></param>
+		private void RaiseselectedItemChanged(T oldItem)
+		{
 			if (SelectedItemChanged != null)
 				SelectedItemChanged.Invoke(this, new ChangedEventArgs<T>(oldItem, SelectedItem));
-			return true;
 		}
 
 		/// <summary>
@@ -98,6 +139,36 @@ namespace Core
 				return (T)Activator.CreateInstance(type);
 			}
 			return null;
+		}
+
+		protected override void ClearItems()
+		{
+			base.ClearItems();
+			ChangeSelectedIndex(-1);
+		}
+
+		protected override void RemoveItem(int index)
+		{
+			if (_selectedIndex != -1 && _selectedIndex == index)
+				throw new InvalidOperationException("Нельзя удалять текущий выделенный элемент. Измените выделенный элемент методами ChangeSelection() или ResetSelection() и выполните операцию повторно.");
+			base.RemoveItem(index);
+			if (_selectedIndex > index)
+				_selectedIndex--;
+		}
+
+		protected override void InsertItem(int index, T item)
+		{
+			base.InsertItem(index, item);
+			if (_selectedIndex >= index)
+				_selectedIndex++;
+		}
+
+		protected override void SetItem(int index, T item)
+		{
+			if(_selectedIndex == index)
+				throw new InvalidOperationException("Нельзя заменить текущий выделенный элемент. Измените выделенный элемент методами ChangeSelection() или ResetSelection() и выполните операцию повторно.");
+			
+			base.SetItem(index, item);
 		}
 	}
 }
