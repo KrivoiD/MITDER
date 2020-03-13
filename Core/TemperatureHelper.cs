c89be010606a6cf6be6a6a8d66fa1c9cb9f79678
Дмитрий Кривченя <krdemetria@ya.ru>
2020-03-13 18:33:56 +0300

    Добавлены комментарии и логика проверок текущей температуры.

--- Core/TemperatureHelper.cs
+++ Core/TemperatureHelper.cs
@@ -7,16 +7,35 @@ namespace Core
 {
 	class TemperatureHelper
 	{
+		/// <summary>
+		/// Действие, вызываемое для измерения сопротивления
+		/// </summary>
 		private Action MeasureResistance;
 		
+		/// <summary>
+		/// Коллекция, содержащая этапы измерения
+		/// </summary>
 		public WSICollection<StepSettings> StepCollection { get; private set; }
 
+		/// <summary>
+		/// Текущая температура (последнее установленное значение)
+		/// </summary>
 		public double CurrentTemperature { get; private set; }
 
+		/// <summary>
+		/// Температура, при которой происходит следующее измерение
+		/// </summary>
+		public double NextTemperature { get; private set; }
+
+		/// <summary>
+		/// Коструктор
+		/// </summary>
+		/// <param name="steps">Коллекция, содержащая этапы измерения</param>
+		/// <param name="measureResistance">Действие, вызываемое для измерения сопротивления</param>
 		public TemperatureHelper(WSICollection<StepSettings> steps, Action measureResistance)
 		{
 			if (steps == null || measureResistance == null)
-				throw new NullReferenceException("Параметры steps/measureResistance не должены быть null.");
+				throw new NullReferenceException("Параметры steps/measureResistance не должны быть null.");
 			if (!steps.Any())
 				throw new ArgumentException("Передаваемая коллекция не должна быть пустой.");
 
@@ -30,6 +49,10 @@ namespace Core
 		/// Указывает на нагрев/охлаждение. Необходим для единной логики проверок.
 		/// </summary>
 		private int _coeff = 1;
+		/// <summary>
+		/// Текущий этап измерения
+		/// </summary>
+		private StepSettings _step = null;
 
 		private void StepCollection_SelectionChanged(WSICollection<StepSettings> collection, ChangedEventArgs<StepSettings> args)
 		{
@@ -47,25 +70,29 @@ namespace Core
 					_coeff = -1;
 					break;
 			}
+			_step = collection.SelectedItem;
 		}
 
 		public void SetCurrentTemperature(double temperature)
 		{
 			CurrentTemperature = temperature;
-			var step = StepCollection.SelectedItem;
-
-			var from = StepCollection.SelectedItem.From;
-			var to = StepCollection.SelectedItem.To;
-
-			//проверяем, вышли за конечную точку этапа
-			if (_coeff * temperature > _coeff * to)
-				StepCollection.ChangeSelection();
-
+			
 			//проверяем, достигли ли отправной точки этапа
-			if (_coeff * temperature < _coeff * from)
+			if (_coeff * temperature < _coeff * _step.From)
 				return;
 
+			//проверяем, надо ли проводить измерение сопротивления
+			if (_coeff * temperature >= _coeff * (NextTemperature - _step.PointRange)
+				&& _coeff * temperature <= _coeff * (NextTemperature + _step.PointRange))
+				MeasureResistance();
 
+			//проверяем, вышли за конечную точку этапа - переходим к следующему этапу
+			if (_coeff * temperature > _coeff * _step.To)
+				StepCollection.ChangeSelection();
+
+			//расчитываем следующую точку для измерения
+			if (_coeff * temperature > _coeff * (NextTemperature + _step.PointRange))
+				NextTemperature += _coeff * _step.Step;
 		}
 	}
 }
