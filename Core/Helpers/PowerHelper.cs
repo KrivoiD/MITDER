31911fd7fc7481d2a902b2a1f9a3d6768048ed23
Дмитрий Кривченя <krdemetria@ya.ru>
2020-10-25 00:34:08 +0300

    Добавлено описание в PowerHelper, убран лишний код

--- Core/Helpers/PowerHelper.cs
+++ Core/Helpers/PowerHelper.cs
@@ -5,16 +5,18 @@ using System.Text;
 
 namespace Core.Helpers
 {
+	/// <summary>
+	/// Класс, следящий за скоростью изменения температуры и указывающий на направление изменения питания.
+	/// Для добавления нового значение использовать <see cref="PowerHelper.AddCurrentTemperature(double)"/>.
+	/// Для получения коэффициента изменения использовать <see cref="PowerHelper.GetPowerChangingDirection"/>
+	/// </summary>
 	public class PowerHelper
 	{
 		private int _interval;
-		private MovableAverage _bottomRateList;
-		private MovableAverage _topRateList;
-		private double _bottomRate;
-		private double _topRate;
+		private MovableAverage _rateList;
+		private double _rate;
 
-		private double _lastBottomTemperature;
-		private double _lastTopTemperature;
+		private double _lastTemperature;
 		private double _temperatureRate;
 		private double _rateRangeStability;
 
@@ -34,7 +36,7 @@ namespace Core.Helpers
 		}
 		/// <summary>
 		/// Диапазон стабильности в мВ/с для <see cref="TemperatureRate"/>. 
-		/// <br /> При попадании текущей скорости изменения температуры в диапазон (<see cref="TemperatureRate"/> +/- указанное значение)Ы не приводит к изменению питания источником.
+		/// <br /> При попадании текущей скорости изменения температуры в диапазон (<see cref="TemperatureRate"/> +/- указанное значение) не приводит к изменению питания источником.
 		/// <br /> По умолчанию установлена 0,0005 мВ/с.
 		/// </summary>
 		public double RateRangeStability
@@ -48,36 +50,37 @@ namespace Core.Helpers
 			}
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="lastValuesAmount">Количество последних показаний, используемых для расчета средней скорости изменения температуры</param>
+		/// <param name="interval">Интервал измерений показаний температур, в секундах</param>
 		public PowerHelper(int lastValuesAmount, int interval)
 		{
 			TemperatureRate = 0.002;
 			RateRangeStability = 0.0005;
 			_interval = interval;
-			_bottomRateList = new MovableAverage(lastValuesAmount);
-			_topRateList = new MovableAverage(lastValuesAmount);
+			_rateList = new MovableAverage(lastValuesAmount);
 		}
 
 		public void AddCurrentTemperature(double bottom)
 		{
-			_bottomRate = _bottomRateList.AddValue((bottom - _lastBottomTemperature) / _interval);
-			_lastBottomTemperature = bottom;
+			_rate = _rateList.AddValue((bottom - _lastTemperature) / _interval);
+			_lastTemperature = bottom;
 		}
 
-		public void AddCurrentTemperature(double bottom, double top)
+		/// <summary>
+		/// Возвращает коэффициент, указывающий на изменение питания.
+		/// </summary>
+		/// <returns>
+		///       1 - необходимо увеличить питание
+		/// <br />0 - оставить без изменений
+		/// <br />-1 - необходимо уменьшить питание</returns>
+		public int GetPowerChangingDirection()
 		{
-			_bottomRate = _bottomRateList.AddValue((bottom - _lastBottomTemperature) / _interval);
-			_lastBottomTemperature = bottom;
-			_topRate = _topRateList.AddValue((top - _lastTopTemperature) / _interval);
-			_lastTopTemperature = top;
-		}
-
-		public int GetPowerChangingDirection(bool forTopRate)
-		{
-			var rate = forTopRate ? _topRate : _bottomRate;
-
-			if (rate < TemperatureRate - RateRangeStability)
+			if (_rate < TemperatureRate - RateRangeStability)
 				return 1;
-			if (rate > TemperatureRate + RateRangeStability)
+			if (_rate > TemperatureRate + RateRangeStability)
 				return -1;
 			return 0;
 		}
