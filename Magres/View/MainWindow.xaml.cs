3a79ca441ef61446fac509cc240d57486d5e0572
Дмитрий Кривченя <krdemetria@ya.ru>
2020-12-09 01:35:02 +0300

    Добавлен новый проект Magres для измерения температурной зависимости магниторезистива.

---
+++ Magres/View/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Magres.ViewModel;
+
+namespace Magres.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Closed += new EventHandler(MainWindow_Closed);
+        }
+
+        void MainWindow_Closed(object sender, EventArgs e)
+        {
+            ((MainWindowViewModel)DataContext).Dispose();
+        }
+    }
+}
